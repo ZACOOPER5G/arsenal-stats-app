b3257280c4a0ed9f14a0e677a68ba9d13c0f7cb8
zac <zacooper5g@outlook.com>
2022-09-05 16:19:54 +1000

    now dynamically displays data from data json

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -1,5 +1,4 @@
 import playerData from '../data/players.json'
-import yomama from '../../src/images/odegaard.jpg'
 
 type PlayerDataProps = {
     name: string
@@ -13,21 +12,14 @@ type PlayerDataProps = {
 }
 
 export const PlayerCard = () => {
-
-    console.log(yomama)
-
     let playerImg = playerData.map(item => item.imageUrl).toString()
     let playerName = playerData.map(item => item.name).toString();
 
     return (
         <div className="container">
             <div className="card">
-                {playerData.map(item => (
-                    <img src={item.imageUrl} alt={item.name}/>
-                ))}
-        
                 <div className="player">
-
+                    <img src={playerImg} alt={playerName}/>
                 </div>
             </div>
         </div>
