575e1d3b034e9fb011dfdb03a9ec0f542fac5721
zac <zacooper5g@outlook.com>
2022-09-05 16:06:06 +1000

    card now reads some data

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -1,12 +1,35 @@
+import playerData from '../data/players.json'
+import yomama from '../../src/images/odegaard.jpg'
 
-
+type PlayerDataProps = {
+    name: string
+    imageUrl: string
+    // position: string
+    // nationality: string
+    // age: string
+    // dob: string
+    // height: string
+    // goals: string
+}
 
 export const PlayerCard = () => {
-  return (
-    <div className="container">
-        <div className="player">
-            Hi
+
+    console.log(yomama)
+
+    let playerImg = playerData.map(item => item.imageUrl).toString()
+    let playerName = playerData.map(item => item.name).toString();
+
+    return (
+        <div className="container">
+            <div className="card">
+                {playerData.map(item => (
+                    <img src={item.imageUrl} alt={item.name}/>
+                ))}
+        
+                <div className="player">
+
+                </div>
+            </div>
         </div>
-    </div>
-  )
+    )
 }
