d51b6e8dd3cacc90a3eb966d44941c3010f5e626
zac <zacooper5g@outlook.com>
2022-09-06 16:31:08 +1000

    added nav logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import { Navbar } from './components/Navbar';
+import { NavItem } from './components/NavItem';
 import './App.css';
+import { PlayerCard } from './components/PlayerCard';
+import { ReactComponent as Arrow} from './images/arrow.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <>
+        <Navbar>
+          <NavItem icon={<Arrow />} />
+        </Navbar>
+        <PlayerCard />
+      </>
+      
     </div>
   );
 }
