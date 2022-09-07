cfc5b5e62da5afd13bb19c62385f3774b3f87f18
zac <zacooper5g@outlook.com>
2022-09-07 10:47:41 +1000

    added state to component

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,9 +1,11 @@
-
+import { useState } from "react"
 
 export const NavItem = (props: any) => {
+    const [open, setOpen]  = useState<Boolean>(false)
+
     return (
       <li className="nav-item">
-          <a href="#" className="icon-button">
+          <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
               {props.icon}
           </a>
       </li>
