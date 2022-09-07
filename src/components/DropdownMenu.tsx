a756b78e3f6d5df2bfa6499133125cda98540d2f
zac <zacooper5g@outlook.com>
2022-09-07 11:34:28 +1000

    adding latest additions before break

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,9 +1,24 @@
+import { ReactComponent as ArsenalLogo} from '../images/arsenal-logo.svg';
+import { ReactComponent as PlayerIcon} from '../images/player-icon.svg';
+import { ReactComponent as Arrow} from '../images/arrow.svg';
 
+const DropdownItem = (props: any) => {
+    return (
+        <a href="#" className="menu-item">
+            <span className="icon-button">{props.leftIcon}</span>
+
+            {props.children}
+
+            <span className="icon-button">{props.rightIcon}</span>
+        </a>
+    )
+}
 
 export const DropdownMenu = () => {
     return (
         <div className="dropdown">
-
+            <DropdownItem leftIcon={<ArsenalLogo/>} rightIcon={<Arrow/>}>Team Stats</DropdownItem>
+            <DropdownItem leftIcon={<PlayerIcon/>} rightIcon={<Arrow/>}>Player Stats</DropdownItem>
         </div>
     )
 }
