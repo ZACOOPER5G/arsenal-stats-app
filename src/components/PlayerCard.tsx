d92a896be50ee41877c1d8d1d1d85371f28bc6ef
zac <zacooper5g@outlook.com>
2022-09-17 19:51:55 +1000

    started adding stats via API

--- src/components/PlayerCard.tsx
+++ src/components/PlayerCard.tsx
@@ -1,3 +1,4 @@
+import axios from 'axios'
 import playerData from '../data/players.json'
 
 export const PlayerCard = (props: any) => {
@@ -8,12 +9,40 @@ export const PlayerCard = (props: any) => {
         }
     })
 
+    let playerKey = playerObj?.id
     let playerImg = playerObj?.imageUrl
-
     let playerName = playerObj?.name
+    let playerPosition = playerObj?.position
+    let playerNationality = playerObj?.nationality
+    let playerAge = playerObj?.age
+    let playerDOB = playerObj?.dob
+    let playerHeight = playerObj?.height
+    let playerGoals = playerObj?.goals
+
+    const options = {
+        method: 'GET',
+        url: `https://footapi7.p.rapidapi.com/api/search/${playerKey}`,
+        headers: {
+          'X-RapidAPI-Key': process.env.REACT_APP_KEY,
+          'X-RapidAPI-Host': 'footapi7.p.rapidapi.com'
+        }
+      };
+
+    const getPlayerData = () => {
+        //@ts-ignore
+        axios.request(options)
+        .then(res => {
+            console.log(res)
+        })
+        .catch(err => {
+            console.log(err)
+        })
+    }
+
 
     return (
         <div className="container">
+            <button onClick={getPlayerData}>click for stats</button>
             <div className="card">
                 <div className="player">
                     <img src={playerImg} alt={playerName} className={playerName} />
@@ -24,27 +53,35 @@ export const PlayerCard = (props: any) => {
                 <div className="stats-container" >
                     <div className="group">
                         <h3 className="label">Position</h3>
-                        <span className="description"></span>
+                        <span className="description">{playerPosition}</span>
                     </div>
                     <div className='group'>
                         <h3 className="label">Nationality</h3>
-                        <span className="description"></span>
+                        <span className="description">{playerNationality}</span>
                     </div>
                     <div className='group'>
                         <h3 className="label">Age</h3>
-                        <span className="description"></span>
+                        <span className="description">{playerAge}</span>
                     </div>
                     <div className='group'>
                         <h3 className="label">DOB</h3>
-                        <span className="description"></span>
+                        <span className="description">{playerDOB}</span>
                     </div>
                     <div className='group'>
                         <h3 className="label">Height</h3>
-                        <span className="description"></span>
+                        <span className="description">{playerHeight}</span>
                     </div>
                     <div className='group'>
                         <h3 className="label">Goals</h3>
-                        <span className="description"></span>
+                        <span className="description">{playerGoals}</span>
+                    </div>
+                    <div className='group'>
+                        <h3 className="label">Goals</h3>
+                        <span className="description">{playerGoals}</span>
+                    </div>
+                    <div className='group'>
+                        <h3 className="label">Goals</h3>
+                        <span className="description">{playerGoals}</span>
                     </div>
                 </div>
             </div>
