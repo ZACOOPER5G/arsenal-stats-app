3bdd4c0cb6bff7a6f039ee98fd5d5ed278391906
zac <zacooper5g@outlook.com>
2022-09-26 16:56:51 +1000

    updated responsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -28,6 +28,10 @@ function App() {
             <div className="card">
               <h1 className='intro-menu'>Use the menu in the top right to select a player!</h1>
               <img className="home-img" src='./images/arsenal-team.png' alt='home-banner' />
+              <div className="stats-container-home">
+                <h1 className='description'>Welcome to the Arsenal Fan Stat Tracker. Select any one of your favourite players from the arsenal starting XI to see how they have been performing this season in the Premier League.</h1>
+                <h1 className='description'>Check back in throughout the season for new updates, and happy tracking!</h1>
+              </div>
             </div>
           </div> 
         }
