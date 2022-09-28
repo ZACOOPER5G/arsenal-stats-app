a7870b89236f9f31d02c0d1049adeed1482d57b8
zac <zacooper5g@outlook.com>
2022-09-28 13:10:25 +1000

    user now able to close dropdown by selecting window

--- src/components/NavItem.tsx
+++ src/components/NavItem.tsx
@@ -1,14 +1,29 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from "react";
 
 export const NavItem = (props: any) => {
-    const [open, setOpen]  = useState<Boolean>(false)
+    const [open, setOpen]  = useState<Boolean>(false);
+
+    const menuRef: any = useRef();
+
+  useEffect(() => {
+    let handler = (event: any) => {
+      if (!menuRef.current?.contains(event.target)) {
+        setOpen(false)
+      }
+    }
+    document.addEventListener("mousedown", handler);
+
+    return () => {
+      document.removeEventListener("mousedown", handler);
+    }
+  }, [])
 
     return (
-      <li className="nav-item">
-          <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
+      <li className="nav-item" ref={menuRef} >
+          <a href="#" className="icon-button" onClick={() => setOpen(!open)} >
               {props.icon}
           </a>
-
+          {/* opens dropdown menu upon clicking chevron */}
           {open && props.children}
       </li>
     )
