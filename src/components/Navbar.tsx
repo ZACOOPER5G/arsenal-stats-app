eff88522ed3026175c6b03d3c8c237cc150f468f
Zac Cooper <zacooper5g@outlook.com>
2022-09-30 16:23:55 +1000

    menu now closes upon selecting player

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,10 @@
 
 
-
-  
-
 export const Navbar = (props: any) => {
   return (
     <nav className="navbar">
         <ul className="navbar-nav">
+            <li className="nav-logo">Arsenal Fan Stat Tracker</li>
             {props.children}
         </ul>
     </nav>
