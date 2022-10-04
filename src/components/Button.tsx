82dbea37e685df0f0c3691f99adbdf0936398a20
Zac Cooper <zacooper5g@outlook.com>
2022-10-04 15:34:49 +1100

    updated button aesthetics as well as responsiveness

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
+import { ReactComponent as Arrow } from '../images/right-arrow.svg'
 
 export const Button = (props: any) => {
   return (
     <span className='pages' >
-        {props.page > 1 && 
-        <button className='page' onClick={props.handleDecrease} >Previous</button>}
-        <h3>Page {props.page}</h3>
-        {props.page < 4 && <button className="page" onClick={props.handleIncrease} >Next</button>}
+        {props.page > 1 && <Arrow className='previous' onClick={props.handleDecrease} >Previous</Arrow>}
+        <h3 className='page' >Page {props.page}</h3>
+        {props.page <= 3 && <Arrow className="next" onClick={props.handleIncrease} >Next</Arrow>}
     </span>
   )
 }
