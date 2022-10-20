6e9390c7a0d4433f8833ebbcfb05fba58405df0f
Zac Cooper <zacooper5g@outlook.com>
2022-10-20 15:03:58 +1100

    updated arsenal card stats display

--- src/components/ArsenalCard.tsx
+++ src/components/ArsenalCard.tsx
@@ -47,43 +47,43 @@ export const ArsenalCard = () => {
                 <div className="stats-container" >
                     <div className="group">
                         <h3 className="label">Goals Leader</h3>
-                        <span className="description">{teamData?.topPlayers.goals['0'].player.shortName}</span>
+                        <span className="description">{teamData?.topPlayers.goals['0'].player.shortName} ({teamData?.topPlayers.goals['0'].statistics.goals})</span>
                     </div>
                     <div className='group'>
                         <h3 className="label">Assists Leader</h3>
-                        <span className="description">{teamData?.topPlayers.assists['0'].player.shortName}</span>
+                        <span className="description">{teamData?.topPlayers.assists['0'].player.shortName} ({teamData?.topPlayers.assists['0'].statistics.assists})</span>
                     </div>
                     <div className='group'>
                         <h3 className="label">Goals/Assists Leader</h3>
-                        <span className="description">{teamData?.topPlayers.goalsAssistsSum['0'].player.shortName}</span>
+                        <span className="description">{teamData?.topPlayers.goalsAssistsSum['0'].player.shortName} ({teamData?.topPlayers.goalsAssistsSum['0'].statistics.goalsAssistsSum})</span>
                     </div>
                     <div className='group'>
                         <h3 className="label">Accurate Pass Leader</h3>
-                        <span className="description">{teamData?.topPlayers.accuratePasses['0'].player.shortName}</span>
+                        <span className="description">{teamData?.topPlayers.accuratePasses['0'].player.shortName} ({teamData?.topPlayers.accuratePasses['0'].statistics.accuratePasses})</span>
                     </div>
                     <div className='group'>
                         <h3 className="label">Ratings Leader</h3>
-                        <span className="description">{teamData?.topPlayers.rating['0'].player.shortName}</span>
+                        <span className="description">{teamData?.topPlayers.rating['0'].player.shortName} ({teamData?.topPlayers.rating['0'].statistics.rating})</span>
                     </div>
                     <div className='group'>
                         <h3 className="label">Tackles Leader</h3>
-                        <span className="description">{teamData?.topPlayers.tackles['0'].player.shortName}</span>
+                        <span className="description">{teamData?.topPlayers.tackles['0'].player.shortName} ({teamData?.topPlayers.tackles['0'].statistics.tackles})</span>
                     </div>
                     <div className='group'>
                         <h3 className="label">Key Passes Leader</h3>
-                        <span className="description">{teamData?.topPlayers.keyPasses['0'].player.shortName}</span>
+                        <span className="description">{teamData?.topPlayers.keyPasses['0'].player.shortName} ({teamData?.topPlayers.keyPasses['0'].statistics.keyPasses})</span>
                     </div>
                     <div className='group'>
                         <h3 className="label">Successful Dribbles Leader</h3>
-                        <span className="description">{teamData?.topPlayers.successfulDribbles['0'].player.shortName}</span>
+                        <span className="description">{teamData?.topPlayers.successfulDribbles['0'].player.shortName} ({teamData?.topPlayers.successfulDribbles['0'].statistics.successfulDribbles})</span>
                     </div>
                     <div className='group'>
                         <h3 className="label">Yellow Card Leader</h3>
-                        <span className="description">{teamData?.topPlayers.yellowCards['0'].player.shortName}</span>
+                        <span className="description">{teamData?.topPlayers.yellowCards['0'].player.shortName} ({teamData?.topPlayers.yellowCards['0'].statistics.yellowCards})</span>
                     </div>
                     <div className='group'>
                         <h3 className="label">Red Card Leader</h3>
-                        <span className="description">{teamData?.topPlayers.redCards === null ? 'None' : teamData?.topPlayers.redCards['0'].player.shortName }</span>
+                        <span className="description">{teamData?.topPlayers.redCards === null ? 'None' : `${teamData?.topPlayers.redCards['0'].player.shortName} (${teamData?.topPlayers.goals['0'].statistics.redCards})`}</span>
                     </div>
                 </div>
             </div>
