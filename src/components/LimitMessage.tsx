c3450b0f59dd2c990dcdd276b0fe1a42740288d8
Zac Cooper <zacooper5g@outlook.com>
2022-11-02 12:06:50 +1100

    updated Vieira image

--- src/components/LimitMessage.tsx
+++ src/components/LimitMessage.tsx
@@ -3,7 +3,7 @@
 export const LimitMessage = () => {
   return (
     <div className="error-msg">
-        <img className='error' src="./images/error.webp"/>Unfortunately, the daily API limit has been reached so data may not be displayed correctly on the page. 
+        <img className='error' src="./images/error.webp"/> Unfortunately, the daily API limit has been reached so data may not be displayed correctly on the page. Check in again tomorrow for accurate and up-to-date statistics!
     </div>
   )
 }
